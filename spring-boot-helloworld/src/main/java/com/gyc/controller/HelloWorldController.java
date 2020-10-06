e9aa84b7dce589c3050b9a8c095971aa7c592334
zichunguo <guozichunvip@163.com>
2020-10-06 17:26:18 +0800

    修改配置文件

--- spring-boot-helloworld/src/main/java/com/gyc/controller/HelloWorldController.java
+++ spring-boot-helloworld/src/main/java/com/gyc/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.gyc.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
+	@Value("${server.port}")
+	private String serverPort;
+
 	@RequestMapping("/hello")
 	public String hello() {
-		return "hello";
+		return "hello" + serverPort;
 	}
 
 }
